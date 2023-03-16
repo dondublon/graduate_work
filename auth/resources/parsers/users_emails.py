1015d857f566c8a53278de5ba2984f526e3e7a0f
RuslanYankov <yankov.rus@gmail.com>
2023-03-16 20:05:36 +0300

    Route users-emails complete

---
+++ auth/resources/parsers/users_emails.py
@@ -0,0 +1,6 @@
+import json
+
+from flask_restful import reqparse
+
+users_id_parser = reqparse.RequestParser()
+users_id_parser.add_argument("users_id", help="This field cannot be blank", required=True, action='append')
---
+++ auth/services/emails_service.py
@@ -0,0 +1,25 @@
+from http import HTTPStatus
+
+from flask import abort
+from sqlalchemy.exc import DataError
+
+from models import User
+
+
+def get_users_emails(users_id: list) -> list:
+    try:
+        user = User.find_all_emails(users_id)
+    except DataError:
+        abort(HTTPStatus.BAD_REQUEST, "Invalid id")
+    else:
+        if not user:
+            abort(HTTPStatus.FORBIDDEN, "Invalid token")
+        return user
+
+
+class EmailService:
+    @classmethod
+    def get_user_roles(cls, users_id: list) -> dict:
+        users = get_users_emails(users_id)
+        emails = {"emails": [user.email for user in users if user.email]}
+        return emails

b2c447cbb5c514ddca35f38abd8fefdddeb853f1
Yaroslav Benin <benin@samawatt.com>
2023-03-17 00:41:43 +0400

    + Notofication pattern

--- notification/notificator/build/db/helper.py
+++ notification/notificator/build/db/helper.py
@@ -4,7 +4,7 @@ import json
 from typing import Iterable
 
 from .connection import connection
-
+from .types_ import NotificationPattern
 
 class InvokationType(IntEnum):
     BY_EVENT = 1
@@ -16,9 +16,9 @@ class DBHelper:
     def __init__(self, connection):
         self.connection = connection
 
-    def choose_event_pattern(self, event_type):
+    def choose_event_pattern(self, event_type) -> NotificationPattern:
         cur = self.connection.cursor()
-        sql = f"SELECT id, pattern_file, actual_time, settings_::json->'event_type' as event_type, settings_" \
+        sql = f"SELECT id, pattern_file, actual_time, settings_" \
               f" FROM notification_pattern" \
               f" WHERE type_={InvokationType.BY_EVENT.value}" \
               f" AND CAST(settings_::json->'event_type' AS VARCHAR) = '\"{event_type}\"'"
@@ -27,7 +27,8 @@ class DBHelper:
         row = cur.fetchone()
         if not row:
             raise ValueError(f"No event pattern for event {event_type}")
-        return row
+        # noinspection PyArgumentList
+        return NotificationPattern(row)
 
 
     def add_notification_event(self, message_id, pattern_id):
@@ -38,14 +39,15 @@ class DBHelper:
         cur.execute(sql, data)
         self.connection.commit()
 
-    def get_time_patterns(self) -> Iterable[dict]:
+    def get_time_patterns(self) -> Iterable[NotificationPattern]:
         sql = f"SELECT * FROM notification_pattern WHERE type_={InvokationType.BY_TIME.value}"
         cur = self.connection.cursor()
         cur.execute(sql)
         result = cur.fetchall()
-        return list(result)
+        # noinspection PyArgumentList
+        return [NotificationPattern(item) for item in result]
 
-    def already_was_msg_id(self, message_id):
+    def already_was_msg_id(self, message_id) -> bool:
         sql = f"SELECT COUNT(*) FROM notification_event WHERE " \
               f"CAST(source::json->'message_id' AS VARCHAR)='\"{message_id}\"'"
         cur = self.connection.cursor()
--- notification/notificator/build/db/types_.py
+++ notification/notificator/build/db/types_.py
@@ -3,3 +3,11 @@ from typing import TypedDict
 
 class User(TypedDict):
     email: str
+
+
+class NotificationPattern(TypedDict):
+    id: str
+    pattern_file: str
+    actual_time: int
+    settings_: dict
+

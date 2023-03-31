5c9acef6f215ac0a67441f6f530c5c94ea7df459
Yaroslav Benin <benin@samawatt.com>
2023-03-31 12:22:55 +0400

    Fix update_protos.sh

--- profiles/update_protos.sh
+++ profiles/update_protos.sh
@@ -1,2 +1,2 @@
 # Install for yourself: pip install grpcio-tools
-python -m grpc_tools.protoc -I./protos --python_out=./src/grpc --pyi_out=./src/grpc --grpc_python_out=./src/grpc ./protos/profiles.proto
+python -m grpc_tools.protoc -I./protos --python_out=./src/grpc_ --pyi_out=./src/grpc_ --grpc_python_out=./src/grpc_ ./protos/profiles.proto

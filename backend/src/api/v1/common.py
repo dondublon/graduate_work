8365894554d437154d9af78ef924d2728fd916b9
RuslanYankov <yankov.rus@gmail.com>
2023-04-11 17:56:41 +0300

    Deleted old decorator

--- backend/src/api/v1/common.py
+++ backend/src/api/v1/common.py
@@ -14,33 +14,6 @@ from fastapi import HTTPException
 from starlette.requests import Request
 
 
-def authorize(func):
-    # Не получилось сделать через декоратор, из-за того, что FastAPI нужна определённая сигнатура.
-    # Можно, но сложно. Этот декоратор не используется,
-    @wraps(func)
-    async def inner(request: Request, **kwargs):
-        data_obj = await request.json()
-        login = data_obj.get("login")
-        password = data_obj.get("password")
-        # noinspection PyUnusedLocal
-        authorization = request.headers.get("Authorization")
-        login_url = f"{settings.auth_protocol_host_port}/api/v1/user/login"
-        auth_response = requests.post(
-            login_url,
-            data=json.dumps({"login": login, "password": password}),
-            headers={"Content-Type": "application/json"},  # "Authorization": authorization,
-        )
-        json_obj = auth_response.json()
-        a_token = json_obj.get("access_token")
-        decoded = jwt.decode(a_token, settings.auth_secret_key, algorithms=["HS256"])
-        if "access_token" in json_obj:
-            return await func(request=request, **kwargs)
-        else:
-            raise HTTPException(HTTPStatus.UNAUTHORIZED, detail="Unauthorized")
-
-    return inner
-
-
 class AuthResult(NamedTuple):
     access_token: str
     user_uuid: str

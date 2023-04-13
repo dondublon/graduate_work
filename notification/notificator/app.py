b963f488a0aedd5133d4438fbc885fc4aff574af
Yaroslav Benin <benin@samawatt.com>
2023-04-13 20:39:27 +0400

    rollback app patch.

--- notification/notificator/app.py
+++ notification/notificator/app.py
@@ -1,8 +1,7 @@
 from build.config import settings, mail_server
 from build.utils.app_factory import create_app
 from build.scheduler import start_scheduler  # <- Shedule starts here
-from gevent import monkey
-monkey.patch_all()
+
 app = create_app(settings)
 smtp_server = mail_server
 

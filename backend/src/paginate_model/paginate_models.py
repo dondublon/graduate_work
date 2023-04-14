c1344a085538fc9fc168f9d98e75b384debe9ec7
RuslanYankov <yankov.rus@gmail.com>
2023-04-14 11:32:06 +0300

    Added comments for review

--- backend/src/paginate_model/paginate_models.py
+++ backend/src/paginate_model/paginate_models.py
@@ -3,7 +3,12 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
+
 class ProfilesOut(BaseModel):
+    """
+    Модель используется в response_model при
+    пагинации метода GET "/user/profiles" (вывод информации о пользователях)
+    """
     id: str
     email: str
     firstName: str

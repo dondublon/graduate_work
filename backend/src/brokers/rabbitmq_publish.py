8393271749a6f08f314b263153815f1bc62c317b
RuslanYankov <yankov.rus@gmail.com>
2023-04-14 03:58:41 +0300

    Block Finally is complete

--- backend/src/brokers/rabbitmq_publish.py
+++ backend/src/brokers/rabbitmq_publish.py
@@ -12,26 +12,30 @@ logger = logging.getLogger(__name__)
 def rabbitmq_publish(rabbitmq_host: str, queue: str, payload: dict):
     rabbitmq = RabbitmqConnection(rabbitmq_host)
     rabbitmq_conn = rabbitmq.init_rabbitmq_connection()
-    rabbitmq_channel = rabbitmq_conn.channel()
+    try:
+        rabbitmq_channel = rabbitmq_conn.channel()
 
-    rabbitmq_channel.queue_declare(queue=queue, durable=True)
+        rabbitmq_channel.queue_declare(queue=queue, durable=True)
 
-    body = {
-        "message_id": generate_uuid(),
-        "payload": payload
-    }
+        body = {
+            "message_id": generate_uuid(),
+            "payload": payload
+        }
 
-    logger.info(f"RABBITMQ body: {body}")
-    rabbitmq_channel.basic_publish(
-        exchange="",
-        routing_key=queue,
-        body=json.dumps(body).encode(),
-        properties=pika.BasicProperties(
-            delivery_mode=2,  # make message persistent
-        ),
-    )
-    rabbitmq_conn.close()
+        logger.info(f"RABBITMQ body: {body}")
+        rabbitmq_channel.basic_publish(
+            exchange="",
+            routing_key=queue,
+            body=json.dumps(body).encode(),
+            properties=pika.BasicProperties(
+                delivery_mode=2,  # make message persistent
+            ),
+        )
+        logger.info(f"{body.get('message_id')} successfully publish to {rabbitmq_host}")
+    except Exception as e:
+        logger.error(f"Message for Rabbit failed. Check error: {e}")
+    finally:
+        rabbitmq_conn.close()
 
-    logger.info(f"{body.get('message_id')} successfully publish to {rabbitmq_host}")
     return
 

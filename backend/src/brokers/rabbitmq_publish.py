5479ee8fcc5563e010fcc6df658a1e0db726d0aa
Yaroslav Benin <benin@samawatt.com>
2023-04-14 10:39:41 +0400

    Continue solving the conflict.

--- backend/src/brokers/rabbitmq_publish.py
+++ backend/src/brokers/rabbitmq_publish.py
@@ -13,29 +13,26 @@ async def rabbitmq_publish(rabbitmq_host: str, queue: str, payload: dict):
     rabbitmq = RabbitmqConnection(rabbitmq_host)
     rabbitmq_conn = await rabbitmq.init_rabbitmq_connection()
     async with rabbitmq_conn:
-        rabbitmq_channel = await rabbitmq_conn.channel()
-
-        await rabbitmq_channel.declare_queue(name=queue, durable=True)
 
         body = {
             "message_id": generate_uuid(),
             "payload": payload
         }
-    try:
-        rabbitmq_channel = rabbitmq_conn.channel()
+        try:
+            rabbitmq_channel = rabbitmq_conn.channel()
 
-        rabbitmq_channel.queue_declare(queue=queue, durable=True)
+            rabbitmq_channel.queue_declare(queue=queue, durable=True)
 
-        logger.info(f"RABBITMQ body: {body}")
-        await rabbitmq_channel.default_exchange.publish(
-            Message(body=json.dumps(body).encode(), delivery_mode=DeliveryMode.PERSISTENT),
-            routing_key=queue
-        )
+            logger.info(f"RABBITMQ body: {body}")
+            await rabbitmq_channel.default_exchange.publish(
+                Message(body=json.dumps(body).encode(), delivery_mode=DeliveryMode.PERSISTENT),
+                routing_key=queue
+            )
 
-        logger.info(f"{body.get('message_id')} successfully published to {rabbitmq_host}")
-    except Exception as e:
-        logger.error(f"Message for Rabbit failed. Queue={queue}, message_id={body.get('message_id')}. Check error: {e}")
-    finally:
-        rabbitmq_conn.close()
-        return
+            logger.info(f"{body.get('message_id')} successfully published to {rabbitmq_host}")
+        except Exception as e:
+            logger.error(f"Message for Rabbit failed. Queue={queue}, message_id={body.get('message_id')}. Check error: {e}")
+        finally:
+            rabbitmq_conn.close()
+            return
 
